d4ac48bc97a146bb47667fbb484351489c5bb9cb
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2026-09-01 17:54:00 +0000

    Update Api.ts

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hyphenconnect.onrender.com/api/v1",
+  baseURL: "https://fhdepotserver.onrender.com/api/v1",
 });
 
 export const signUp = async (payload: any) => {
